a99852566d7c45533e5fbe3e13870fdddacdfbe9
GabrielNhambirri <gabasboy2@gmail.com>
2022-01-02 14:49:46 +0200

    Apague uma parte do codigo

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package programa;
 
 import java.io.IOException;
@@ -14,11 +10,9 @@ import programa.Equipe_Lista;
  */
 public class Programa {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(final String[] args)throws IOException {
-        // TODO code application logic here     final Equipe_Lista listaEquipe = new Equipe_Lista();
+        
         final Equipe_Lista listaEquipe = new Equipe_Lista();
         listaEquipe.inserirNome_Coach();
         listaEquipe.inserirNome();
